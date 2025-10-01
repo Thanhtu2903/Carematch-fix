b112c30e6e2bb09c8b4f6547634607523b854498
Thanhtu2903 <60025986+Thanhtu2903@users.noreply.github.com>
2025-09-30 21:40:39 -0400

    Update carematch_dashboard.py

--- carematch_dashboard.py
+++ carematch_dashboard.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import matplotlib.pyplot as plt 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 st.markdown(""" ***GROUP 4***: TU PHAM & MINH NGUYEN""")
 # === Dashboard Title ===
 st.title("📊 Carematch Dashboard")
